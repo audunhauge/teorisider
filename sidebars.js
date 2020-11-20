25facbfa97db6023073fa98d973183f8e2d2fd1a
Audun Hauge <audun.hauge@gmail.com>
2020-11-20 15:10:32 +0100

    making stuff automagical

--- sidebars.js
+++ sidebars.js
@@ -1,11 +1,23 @@
+const fs = require('fs');
+const { listenerCount } = require('process');
+
+async function print(path) {
+  const dir = await fs.promises.opendir(path);
+  const list = [];
+  for await (const dirent of dir) {
+    if(!dirent.isDirectory())  list.push(dirent.name);
+  }
+  console.log(list);
+}
+print('./docs/Planlegging').catch(console.error);
 module.exports = {
   someSidebar: {
-    JSTeori: (`variable,string,array,lokker,`
+    JSTeori: (`variable,string,JStheory/array,lokker,`
       + `funksjoner,beregninger,betingelser,objects,klasser,`
-      + `mapset,praktisk,testing,skjema,filformat,`
+      + `mapset,praktisk,Planlegging/testing,skjema,filformat,`
       + `webapi,canvas,custom`).split(","),
-    Planlegging: ('firesteg,kravspek,planlegging,'
-    +'testing,dokumentasjon'
+    Planlegging: ('Planlegging/firesteg,Planlegging/kravspek,Planlegging/planlegging,'
+    +'Planlegging/testing,Planlegging/dokumentasjon'
     +'').split(","),
     Sammendrag: ['uke34',
     'uke35',
