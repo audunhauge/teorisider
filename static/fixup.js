276d85a8ce8b9d16ffde4a2d4a385d1f3262aa60
audun hauge <audun.hauge@gmail.com>
2021-01-28 19:30:09 +0100

    hashchange

--- static/fixup.js
+++ static/fixup.js
@@ -1,5 +1,5 @@
 (function fixup() {
-    document.addEventListener("DOMContentLoaded", function(){
+    document.addEventListener("hashchange", function(){
         const mathLike = document.querySelectorAll("div.language-math div.token-line");
         mathLike.forEach(e => katex.render(e.innerText,e));
     });
