beb44cb47ec05b85da1f71602bd18d217398fb5d
audun hauge <audun.hauge@gmail.com>
2021-01-28 19:37:51 +0100

    fix fix history pushstate

--- static/fixup.js
+++ static/fixup.js
@@ -1,6 +1,8 @@
-(function fixup() {
-    document.addEventListener("hashchange", function(){
-        const mathLike = document.querySelectorAll("div.language-math div.token-line");
-        mathLike.forEach(e => katex.render(e.innerText,e));
-    });
-}())
+(function(history){
+    var pushState = history.pushState;
+    history.pushState = function(state) {
+      const mathLike = document.querySelectorAll("div.language-math div.token-line");
+      mathLike.forEach(e => katex.render(e.innerText,e));
+      return pushState.apply(history, arguments);
+    };
+})(window.history);
